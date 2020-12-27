2800bd5fb263ef99162814b990b11b1c738c5038
Omega Terra Coriel <44127678+MissCoriel@users.noreply.github.com>
2020-12-27 17:39:47 -0600

    Updated to 1.3.0

--- SitnRelax/SitnRelax/ModEntry.cs
+++ SitnRelax/SitnRelax/ModEntry.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Threading;
 using Microsoft.Xna.Framework;
@@ -13,11 +13,12 @@ namespace SitnRelax
 {
     public class ModEntry : Mod
     {
-        
-        private int SecondsUntilRegen;
-        private bool initialSit = false;
+
+        private readonly PerScreen<int> SecondsUntilRegen = new PerScreen<int>();
+        private readonly PerScreen<bool> initialSit = new PerScreen<bool>();
+        private readonly PerScreen<string> farmerName = new PerScreen<string>();
         private configModel Config;
-        //public PerScreen<int> sittingHP;
+        private readonly PerScreen<int> sittingHP = new PerScreen<int>();
 
         
         public override void Entry(IModHelper helper)
@@ -49,59 +50,65 @@ namespace SitnRelax
         {
             if (!Game1.player.isSitting)
             {
-                this.SecondsUntilRegen = this.Config.regenRate;
-                initialSit = false;
+                this.SecondsUntilRegen.Value = this.Config.regenRate;
+                initialSit.Value = false;
                 return;
             }
 
             // wait until regen time
-            if (this.SecondsUntilRegen > 0)
+            if(Game1.player.hasMenuOpen == false)
             {
-                if (initialSit == false)
+                if (this.SecondsUntilRegen.Value > 0)
                 {
-                    Game1.addHUDMessage(new HUDMessage($"{Game1.player.name} {Config.restMessage}", 4));
-                   // sittingHP = Game1.player.health;
-                    initialSit = true;
+                    if (initialSit.Value == false)
+                    {
+                        this.farmerName.Value = Game1.player.Name;
+                        Game1.addHUDMessage(new HUDMessage($"{Game1.player.name} {Config.restMessage}", 4));
+                        // sittingHP = Game1.player.health;
+                        initialSit.Value = true;
+                    }
+
+                    if (e.IsOneSecond)
+                        this.SecondsUntilRegen.Value--;
+
+                    if (this.SecondsUntilRegen.Value > 0)
+                        return;
                 }
+                //apply regen 
+                if (Game1.player.stamina >= Game1.player.maxStamina.Value && Game1.player.health >= Game1.player.maxHealth)
+                {
+                    Monitor.Log("Stamina and Health are full.. doing nothing...", LogLevel.Trace);
 
-                if (e.IsOneSecond)
-                    this.SecondsUntilRegen--;
-
-                if (this.SecondsUntilRegen > 0)
-                    return;
-            }
-            //apply regen 
-            if (Game1.player.stamina >= Game1.player.maxStamina.Value && Game1.player.health >= Game1.player.maxHealth)
-            {
-                Monitor.Log("Stamina and Health are full.. doing nothing...", LogLevel.Trace);
-
-            }
-            else if (Game1.player.stamina < Game1.player.maxStamina.Value)
+                }
+                else if (Game1.player.stamina < Game1.player.maxStamina.Value)
                 {
                     Game1.player.stamina = Math.Min(Game1.player.stamina + this.Config.stamRegen, Game1.player.maxStamina.Value);
                     Monitor.Log("Regenerating Stamina", LogLevel.Trace);
-                    SecondsUntilRegen = this.Config.regenRate;
+                    SecondsUntilRegen.Value = this.Config.regenRate;
 
                 }
-            else if (Game1.player.stamina >= Game1.player.maxStamina.Value)
-            {
+                else if (Game1.player.stamina >= Game1.player.maxStamina.Value)
+                {
                     if (Game1.player.health < Game1.player.maxHealth)
                     {
                         Game1.player.health = Game1.player.health + this.Config.healthRegen;
                         Monitor.Log("Stamina Full... Regenerating Health", LogLevel.Trace);
-                        SecondsUntilRegen = this.Config.regenRate;
+                        SecondsUntilRegen.Value = this.Config.regenRate;
                     }
+                }
+                if (Game1.player.health < sittingHP.Value)
+                {
+                    double damageTaken = sittingHP.Value - Game1.player.health;
+                    double addDamage = damageTaken * .50;
+                    Game1.player.takeDamage((int)addDamage, false, null);
+                    Game1.addHUDMessage(new HUDMessage("Hey!! What are you doing?! Don't let your guard down!!", 3));
+                    Game1.player.isStopSitting = true;
+                    Game1.player.currentTemporaryInvincibilityDuration = 0;
+                    sittingHP.Value = Game1.player.health;
+                }
+
             }
-            /*if (Game1.player.health < sittingHP)
-            {
-                double damageTaken = sittingHP - Game1.player.health;
-                double addDamage = damageTaken * .50;
-                Game1.player.takeDamage((int)addDamage, false, null);
-                Game1.addHUDMessage(new HUDMessage("Hey!! What are you doing?! Don't let your guard down!!", 3));
-                Game1.player.isStopSitting = true;
-                Game1.player.currentTemporaryInvincibilityDuration = 0;
-                sittingHP = Game1.player.health;
-            }*/
+
 
             
         }
